c5c19ddf166e0c59130911dfe04dd6125ef33e46
lyn7568 <1271778060@qq.com>
2019-03-06 17:35:02 +0800

    first commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [HomeComponent, DashboardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
